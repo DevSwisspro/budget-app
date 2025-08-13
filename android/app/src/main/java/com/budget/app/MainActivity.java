48c8159b0d1450de80aa2254ff5a3826a7118536
Akuma-Secure <akuma@secure.local>
2025-08-13 21:58:33 +0200

    Final: Configuration Android optimisée pour déploiement

--- android/app/src/main/java/com/budget/app/MainActivity.java
+++ android/app/src/main/java/com/budget/app/MainActivity.java
@@ -1,12 +1,5 @@
 package com.budget.app;
 
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+public class MainActivity extends BridgeActivity { }
